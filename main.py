1a715cea75b42e6f71040c770f2110d58f9502b6
checkro020 <meusportetotal@gmail.com>
2025-07-06 23:30:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Adicionando o middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["https://premioverde.com"] para mais segurança
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
 @app.get("/")
 def read_root():
     return {"message": "Bem-vindo à API de Análise de Sites"}
+
