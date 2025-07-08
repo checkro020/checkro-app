966a69a638f1bf91c9a7286284280b659cc43f22
checkro020 <meusportetotal@gmail.com>
2025-07-08 09:35:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://checkro.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
